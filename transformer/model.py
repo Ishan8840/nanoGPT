d2e5f214bd2850a5174378cc296c9edf5dcb7438
Ishan Shah <ishah8840@gmail.com>
2026-04-27 19:43:43 -0400

    added batch generation

--- transformer/model.py
+++ transformer/model.py
@@ -6,5 +6,30 @@ with open('input.txt', 'r') as f:
     text = f.read()
 
 
-print(f"Total Chars: {len(text)}")
-print(text[:100])
+chars = ''.join(sorted(set(text)))
+
+# encoding and decoding
+stoi = { ch:i for i, ch in enumerate(chars)}
+itos = { i:ch for i, ch in enumerate(chars)}
+
+encoded = torch.tensor([stoi[c] for c in text], dtype=torch.long)
+
+
+def get_batch(batch_size, chunk_size):
+    x = torch.zeros([batch_size, chunk_size], dtype=torch.long)
+    y = torch.zeros([batch_size, chunk_size], dtype=torch.long)
+
+    for i in range(batch_size):
+        idx = torch.randint(0, len(encoded) - chunk_size, (1,))
+        xb = encoded[idx:idx+chunk_size]
+        yb = encoded[idx+1:idx+chunk_size+1]
+        x[i] = xb
+        y[i] = yb
+    
+    return x, y
+
+
+x, y = get_batch(4, 8)
+
+print(x[1])
+print(y[1])
